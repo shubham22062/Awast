d6a061cb01f799c6e2999dcd150f495c28adbc7b
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-21 18:14:58 +0530

    fix

--- landing/components/Navbar.tsx
+++ landing/components/Navbar.tsx
@@ -1,8 +1,8 @@
 export default function Navbar(){
     return(
-        <nav className="flex justify-between">
+        <nav className="flex justify-between p-4">
             <h1 className="font-bold text-2xl">Awast</h1>
-          <div className="border rounded-full bg-black/40 backdrop-blur-md p-2 flex gap-3 items-center">
+          <div className="border rounded-full bg-black/40 backdrop-blur-md p-2 flex gap-4 items-center">
             <span className="text-white hover:underline">Home</span>
           <span className="text-white hover:underline">Pricing</span>
           <span className="text-white hover:underline">Features</span>
@@ -10,8 +10,10 @@ export default function Navbar(){
           <span className="text-white hover:underline">About</span>  
             
           </div>
-          <span>Learn More</span>
+          <div className="flex gap-3">
+            <span>Learn More</span>
           <button className="border rounded-md p-3 bg-black text-white">Get Started</button>
+          </div>
         </nav>
     )
 }
