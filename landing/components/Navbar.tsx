b2f4eea8ed8fd69da8687fc2d0b0869af6b554c9
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-22 23:29:27 +0530

    change the navbar

--- landing/components/Navbar.tsx
+++ landing/components/Navbar.tsx
@@ -1,19 +1,51 @@
-export default function Navbar(){
-    return(
-        <nav className="flex justify-between p-4">
-            <h1 className="font-bold text-2xl">Awast</h1>
-          <div className="border rounded-full bg-black/40 backdrop-blur-md p-2 flex gap-4 items-center">
-            <span className="text-white hover:underline">Home</span>
-          <span className="text-white hover:underline">Pricing</span>
-          <span className="text-white hover:underline">Features</span>
-          <span className="text-white hover:underline">Conatct</span>
-          <span className="text-white hover:underline">About</span>  
-            
-          </div>
-          <div className="flex gap-3">
-            <span>Learn More</span>
-          <button className="border rounded-md p-3 bg-black text-white">Get Started</button>
-          </div>
-        </nav>
-    )
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-4 fixed top-0 w-full z-50">
+      
+      {/* Logo */}
+      <Image
+        src="/Logo.svg"
+        alt="Logo"
+        width={100}
+        height={100}
+        className="object-contain"
+      />
+
+      {/* Center Menu */}
+      <div className="border border-white/10 rounded-full bg-black/70 backdrop-blur-md px-6 py-3 flex gap-6 items-center">
+        <span className="text-white hover:underline cursor-pointer">
+          Home
+        </span>
+
+        <span className="text-white hover:underline cursor-pointer">
+          Pricing
+        </span>
+
+        <span className="text-white hover:underline cursor-pointer">
+          Features
+        </span>
+
+        <span className="text-white hover:underline cursor-pointer">
+          Contact
+        </span>
+
+        <span className="text-white hover:underline cursor-pointer">
+          About
+        </span>
+      </div>
+
+      {/* Right Side */}
+      <div className="flex gap-4 items-center">
+        <span className="cursor-pointer">
+          Learn More
+        </span>
+
+        <button className="rounded-md px-5 py-3 bg-black text-white font-medium  transition">
+          Get Started
+        </button>
+      </div>
+    </nav>
+  );
 }
