e337cfde82d476271a42b74dc04166f0abe25803
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-23 16:58:18 +0530

    changes

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
   return (
-  <div>
-    <video className="w-full h-full"
+  <div className="relative w-full h-screen overflow-hidden">
+    <video className="wabsolute top-0 left-0 w-full h-full object-cover"
     autoPlay
     muted
     loop
@@ -13,6 +14,7 @@ export default function Home() {
       <source src="https://res.cloudinary.com/dbdvicxun/video/upload/v1779534486/11353720-uhd_3840_2160_60fps_1_jpb5vo.mp4" type="video/mp4"/>
     </video>
     <Navbar/>
+    <Hero/>
   </div>
   );
 }
---
+++ landing/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return(
+        <div className="items-center">
+            <h1 className="text-6xl text-shadow-gray-400"> Empower Your Future With Intelligent Automation</h1>
+        </div>
+    )
+}
