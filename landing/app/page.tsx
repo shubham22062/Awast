72b30d482b0406da5b7e9d031608f1233f3d3b76
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-23 16:42:26 +0530

    changes

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     loop
     playsInline
     >
-      <source src="/landing.mp4" type="video/mp4"/>
+      <source src="https://res.cloudinary.com/dbdvicxun/video/upload/v1779534486/11353720-uhd_3840_2160_60fps_1_jpb5vo.mp4" type="video/mp4"/>
     </video>
     <Navbar/>
   </div>
