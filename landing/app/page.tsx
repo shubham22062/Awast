2285698a2a722a6a7966e469ee40023baf756374
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-23 15:57:32 +0530

    adding gif

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -4,6 +4,14 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
   <div>
+    <video className="w-full h-full"
+    autoPlay
+    muted
+    loop
+    playsInline
+    >
+      <source src="/landing.mp4" type="video/mp4"/>
+    </video>
     <Navbar/>
   </div>
   );
