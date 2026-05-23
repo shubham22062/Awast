1ecfb7602e213550e363b882788b17ca6a050e77
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-23 15:58:48 +0530

    changes

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
   <div>
-    <video className="w-full h-full"
+    <video className="w-full h-screen"
     autoPlay
     muted
     loop
