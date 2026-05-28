d75ce4b604f5d65ef425153262cdd22d8e927b24
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-28 13:56:39 +0530

    cards

--- landing/components/Features.tsx
+++ landing/components/Features.tsx
@@ -14,7 +14,7 @@ export default function Features(){
             <h1 className="text-3xl text-gray-100 pt-3">Innovative Solutions at Your Fingertips</h1>
             <p className="text-gray-400 pt-3 max-w-xl">Explore our powerful chatboat features that simplify task, automate process, and provode intelligent interaction for seamless communiocation</p>
 
-            <div className="pt-5 flex">
+            <div className="pt-5">
                 <button className=" flex border rounded-md bg-gray-100 p-2 gap-2 transition-transform duration-300 hover:scale-105">
                   
                     Explore Features
@@ -24,6 +24,83 @@ export default function Features(){
                 </button>
                 
 
+            </div>
+
+            <div className="pt-10 grid grid-cols-4 grid-rows-1 gap-4">
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+            <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                <div className="border border-teal-300 rounded-md w-[300px] h-[200px] p-4 transition-transform duration-300 hover:scale-105">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-15 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                    <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
+                    <p className="text-gray-400 text-sm max-w-lg">It's as easy as using an Apple, but not as expensive as buying one.</p>
+
+                </div>
+
+                  
             </div>
         </div>
     )
