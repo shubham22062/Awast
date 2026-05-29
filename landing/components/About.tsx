35d76319408e6a8c4c1e2ac35e5c811c68360f57
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-29 23:46:40 +0530

    about

--- landing/components/About.tsx
+++ landing/components/About.tsx
@@ -1,9 +1,10 @@
 export default function About(){
     return (
-        <div className="justify-center text-center items-center py-30">
+        <div className="justify-center text-center items-center pt-30">
             <span className="text-white text-7xl">Loved by</span>
             <br></br>
             <span className="text-teal-300 text-7xl">people everywhere</span>
+            <p className="text-gray-400 text-xl pt-6">Join thousands of satisfied users who have transformed their workflow</p>
         </div>
     )
 }
