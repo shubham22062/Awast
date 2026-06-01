e7db47691d1689b8b0576076f0caef46a65c5abd
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-01 22:25:53 +0530

    add route

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -6,6 +6,9 @@ app.get("/", (req,res)=>{
     res.send("This is is your backend")
 });
 
+app.get("/ai page",(req,res)=>{
+    res.send("This is the main page")
+})
 app.listen(4000,()=>{
     console.log("server is running")
 })
