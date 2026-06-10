ab60e90ae37ce8052a15f088c5aefb935ab35f3b
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-10 23:25:01 +0530

    changes

--- landing/components/About.tsx
+++ landing/components/About.tsx
@@ -10,9 +10,7 @@ export default function About(){
 
                 {/* Card 1 */}
                 <div className="border border-teal-300 rounded-md w-full min-h-[200px] p-4 transition-transform duration-300 hover:scale-105 bg-transparent">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-11 text-white border border-teal-300 rounded-md p-2 transition-transform duration-300 hover:scale-105">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                    </svg>
+                <span className="block rounded-full bg-teal-300 border border-gray-400 text-white text-xl w-8 h-8">S</span>
                     <span className="text-2xl text-gray-100 font-bold block pt-5">Easy to use</span>
                     <p className="text-gray-400 text-sm mt-1">It's as easy as using an Apple, but not as expensive as buying one.</p>
                 </div>
